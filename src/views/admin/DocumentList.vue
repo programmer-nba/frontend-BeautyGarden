a94f45008b59ab350d76571a965ccce97104325e
KieKoongDev <kiekoong.dev@gmail.com>
2024-02-02 15:40:22 +0700

    fds
    sdf

--- src/views/admin/DocumentList.vue
+++ src/views/admin/DocumentList.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="flex flex-wrap mt-4">
+    <div class="flex flex-col mt-4">
       <button class="px-2 bg-black text-white" @click.prevent="showTable">switch</button>
+      <div class="w-full mb-12 px-4" style="z-index:1000;">
+        <card-quotations color="light" />
+      </div>
       <div v-if="isVat" class="w-full mb-12 px-4">
         <card-receipts color="dark" @changeToNoVat="isVat = !isVat"/>
       </div>
       <!-- <div v-if="!isVat" class="w-full mb-12 px-4">
         <CardNoVatReceipts color="dark" @changeToVat="isVat = !isVat"/>
       </div> -->
-      <div class="w-full mb-12 px-4">
-        <card-quotations color="light" />
-      </div>
       <!-- <div class="w-full mb-12 px-4">
         <card-invoices color="light" />
       </div> -->
